3d3e3df28b8b4a41e0193f8d3c1762767c2b942e
Samuel Silva <samuelsilvawb@gmail.com>
2020-06-19 23:38:46 +0100

    refactor: Update step to include descriptions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const { argv } = require('yargs');
 
 const steps = require('./steps');
+const logger = require('./utils/logger');
 
 (() => {
 	const { user, pass } = argv;
 
+	logger().warn("\nWelcome, let's begin!");
+
 	Object.keys(steps).reduce((promisesChain, key) => {
-		return promisesChain.then((context) => {
-			return steps[key](context);
-		});
+		return promisesChain
+			.then((context) => {
+				const { describe, fn } = steps[key];
+
+				logger().success(`Step ${key} - ${describe}...`);
+
+				return fn(context);
+			})
+			.catch((error) => {
+				logger().error(error);
+				logger().warn('\n Unfortunately we will have to stop here. :(');
+
+				process.exit(0);
+			});
 	}, Promise.resolve({ user, pass }));
 })();
--- src/steps/index.js
+++ src/steps/index.js
@@ -1,12 +1,36 @@
 const steps = {
-	1: require('./1-sign-in'),
-	2: require('./2-select-date'),
-	3: require('./3-add-or-edit'),
-	4: require('./4-select-client-and-project'),
-	5: require('./5-insert-hours'),
-	6: require('./6-save-timesheet'),
-	7: require('./7-reopen-timesheet'),
-	8: require('./8-submit-timesheet'),
+	1: {
+		describe: 'Doing sign-in',
+		fn: require('./1-sign-in'),
+	},
+	2: {
+		describe: 'Selecting year and month',
+		fn: require('./2-select-date'),
+	},
+	3: {
+		describe: 'Adding Timesheet',
+		fn: require('./3-add-or-edit'),
+	},
+	4: {
+		describe: 'Selecting client and project',
+		fn: require('./4-select-client-and-project'),
+	},
+	5: {
+		describe: 'Filling hours',
+		fn: require('./5-insert-hours'),
+	},
+	6: {
+		describe: 'Saving Timesheet',
+		fn: require('./6-save-timesheet'),
+	},
+	7: {
+		describe: 'Re opening Timesheet to submit',
+		fn: require('./7-reopen-timesheet'),
+	},
+	8: {
+		describe: 'Submitting Timesheet',
+		fn: require('./8-submit-timesheet'),
+	},
 };
 
 module.exports = steps;
