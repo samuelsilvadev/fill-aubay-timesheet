96988eb0f14ba7c02538dabdff31795f0d0a3c71
Samuel Silva <samuelsilvawb@gmail.com>
2020-06-19 20:24:56 +0100

    feat: Add complete flow to finish a timesheet

---
+++ src/utils/months.js
@@ -0,0 +1,18 @@
+const MONTHS_MAP_INDEX_PT = [
+	{ month: 'janeiro', index: 1 },
+	{ month: 'fevereiro', index: 2 },
+	{ month: 'março', index: 3 },
+	{ month: 'abril', index: 4 },
+	{ month: 'maio', index: 5 },
+	{ month: 'junho', index: 6 },
+	{ month: 'julho', index: 7 },
+	{ month: 'agosto', index: 8 },
+	{ month: 'setembro', index: 9 },
+	{ month: 'outubro', index: 10 },
+	{ month: 'novembro', index: 11 },
+	{ month: 'dezembro', index: 12 },
+];
+
+module.exports = {
+	MONTHS_MAP_INDEX_PT,
+};
